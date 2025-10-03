c56df35a236439dd0eecf103dbeba4444a86dfed
Felipe Fonseca <fefonsecaa14@gmail.com>
2025-10-02 22:14:34 -0300

    finishing modified version

--- Card-8/pratica/modificacao_pratica.py
+++ Card-8/pratica/modificacao_pratica.py
@@ -1,59 +1,158 @@
 from bs4 import BeautifulSoup
 import requests
-import time
+import os
+import platform
 
-# Pedindo uma skill para o usuário
-print("Put some skill that you are not familiar with")
-
-# Pega o valor que o usuário desseja passar como skill
-unfamiliar_skill = input('>')
-
-print(f"Filtering out {unfamiliar_skill}")
-
-def find_jobs():
+# função para puxar o html
+def get_html(url):
 
     # Requisitando o html do site 
-    html_Text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=ft&searchTextText=Python&txtKeywords=Python%2C&txtLocation=').text
+    html_Text = requests.get(url).text
 
     # Utilizando o soup para ler o html
     soup = BeautifulSoup(html_Text, 'lxml')
 
+    return soup
+
+# função para achar os empregos
+def find_jobs(soup):
+
+    job_list = []
+
     # Achando todos os "cards" de emprego
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
-    for index, job in enumerate(jobs):
+    for job in jobs:
 
-        # pegando a data de publicação do span
-        published_date = job.find('span', class_ = 'sim-posted').span.text.replace(' ','')
-        if 'few' in published_date:
+        # achando o nome da compania desse primeiro emprego. 
+        # Utilizei agora a funcão join para fazer a limpeza correta e separar corretamente o nome da empresa.
+        company_name = ' '.join(job.find('h3', class_ = 'joblist-comp-name').text.split())
+
+        # achando as skills desse emprego.
+        # Arrumei aqui também o espaçamento com a mesma função.
+        skills = ' '.join(job.find('div', class_ = 'more-skills-sections').text.split())
+
+        # pegando o link de mais informações
+        more_info = job.header.h2.a['href']
+
+        # salvando em um dicionário agora
+        job_dic = {
+            'name': company_name,
+            'skills': skills,
+            'info': more_info
+
+        }
+
+        job_list.append(job_dic)
+        save_file(job_dic)
+
+    return job_list
+
+    
+# função que monta a interface inicial
+def interface(jobs):
+
+    clear()
+
+    print('What filter do you want to use?\n\n')
+    print('0 - exit program')
+    print('1 - familiar skills')
+    print('2 - unfamiliar skill')
+    
+    r = int(input(' > '))
+
+    # escolhe uma opção
+    match r:
+        case 0:
+
+            print('Exiting program...')
+            return 0
+        
+        case 1:
+
+            print('Enter a familiar skill')
+            skill = input(' > ')
+            filter(jobs, 1, skill)
+            return 1
+            
+        
+        case 2: 
+
+            print('Enter a unfamiliar skill')
+            skill = input(' > ')
+            filter(jobs, 2, skill)
+
+            return 2
+        
+        case _:
+            print('Invalid value')
+            return -1
+        
+# Limpa o terminal
+def clear():
+
+    system = platform.system()
+    if system == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# função que busca com filtro na skill
+def filter(jobs, filter, skill):
+
+    # para cada emprego
+    for job in jobs:
+
+        # verifica o tipo do filtro
+
+        if filter == 1:  # familiar skills
+
+            # verifica se emprego bate com o filtro
+            if skill.lower() in job['skills'].lower(): # lower para melhor verificação
+
+                print(f"Company name: {job['name']}")
+                print(f"Required Skills: {job['skills']}")
+                print(f"More info: {job['info']}\n")
+
+        elif filter == 2:  # unfamiliar skill
+
+            if skill.lower() not in job['skills'].lower():
+
+                print(f"Company name: {job['name']}")
+                print(f"Required Skills: {job['skills']}")
+                print(f"More info: {job['info']}\n")
+
+    input('Aperte alguma tecla para continuar...')
 
 
-            # achando o nome da compania desse primeiro emprego. 
-            # Utilizei agora a funcão join para fazer a limpeza correta e separar corretamente o nome da empresa.
-            company_name = ' '.join(job.find('h3', class_ = 'joblist-comp-name').text.split())
+# função para salvar os dados em um arquivo
+def save_file(job):
 
-            # achando as skills desse emprego. OBS: diferente da aula, o site nao utiliza mais span, e sim div para guardar as skils, e o nome também está diferente.
-            skills = ' '.join(job.find('div', class_ = 'more-skills-sections').text.split())
+    with open(f'posts/jobs.txt', 'a') as f:
 
-            # pegando o link de mais informações
-            more_info = job.header.h2.a['href']
+        f.write(f"Company name: {job['name'].strip()} \n")
+        f.write(f"Required Skills: {job['skills'].strip()} \n")
+        f.write(f'More info: {job['info']}\n')
 
-            # se a skill que não é familiar para o usuário, não está no emprego, ela não aparece
-            if unfamiliar_skill not in skills:
+        f.write('\n')
 
-                with open(f'posts/{index}.txt', 'w') as f:
+        print("Saving to:", os.path.abspath("posts/jobs.txt"))
 
 
-                    f.write(f"Company name: {company_name.strip()} \n")
-                    f.write(f"Required Skills: {skills.strip()} \n")
-                    f.write(f'More info: {more_info}\n')
-
-                print("File saved: {index}")
 
 # Se esse foi o arquivo main na abertura do programa
 if __name__ == '__main__':
+
+    # url para buscar
+    url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=ft&searchTextText=Python&txtKeywords=Python%2C&txtLocation='
+    
+    # chama as funções
+    soup = get_html(url)
+    jobs = find_jobs(soup)
+
     while True:
-        find_jobs()
-        time_wait = 10
-        print(f'Waiting {time_wait} minutes...')
-        time.sleep(time_wait * 60)
+
+        # só fecha se o usuário quiser fechar o programa
+        r = interface(jobs)
+        if r == 0:
+            break
