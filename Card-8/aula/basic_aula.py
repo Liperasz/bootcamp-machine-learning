255036d30624bd22c69f5942014e2992599b2211
Felipe Fonseca <fefonsecaa14@gmail.com>
2025-10-04 21:30:53 -0300

    Add comments in basic_aula.py

--- Card-8/aula/basic_aula.py
+++ Card-8/aula/basic_aula.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 
+# abrindo o arquivo html com a função "read"
 with open('home.html', 'r') as html_file:
 
+    "função que lê o html"
     content = html_file.read()
 
+    # lendo o arquivo html como lxml agora, através da beautifulsoup, que organiza melhor a leitura
     soup = BeautifulSoup(content, 'lxml')
      
+    # achando todos os cards
     course_cards = soup.find_all('div', class_='card')
 
     for course in course_cards:
+        # printando o nome do curso e o preço
         course_name = course.h5.text
         course_price = course.a.text.split()[-1]
 
