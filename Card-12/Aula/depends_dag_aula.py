f953c1debc0fbd59dc70e469371af8febc254462
Felipe Fonseca <fefonsecaa14@gmail.com>
2025-12-22 20:28:49 -0300

    add comments in code 2

--- Card-12/Aula/depends_dag_aula.py
+++ Card-12/Aula/depends_dag_aula.py
@@ -5,11 +5,14 @@ from airflow.operators.dummy_operator import DummyOperator
 
 from datetime import datetime, timedelta
 
+# argumentos padrão das tarefas do DAG
 default_args = {
-    'start_date': datetime(2019, 1, 1),
+    'start_date': datetime(2025, 1, 1),
     'owner': 'Airflow'
 }
 
+# funções das tarefas 2 e 3
+# a linha comentada serve para simular uma falha na tarefa
 def second_task():
     print('Hello from second_task')
     # raise ValueError('This will turns the python task in failed state')
@@ -20,13 +23,14 @@ def third_task():
 
 with DAG(dag_id='depends_task', schedule_interval="0 0 * * *", default_args=default_args) as dag:
     
-    # Task 1
+    # tarefa 1
+    # wait for downstream faz com que a tarefa espere todas as tarefas dependentes serem concluídas antes de marcar como concluída
     bash_task_1 = BashOperator(task_id='bash_task_1', bash_command="echo 'first task'", wait_for_downstream=True)
     
-    # Task 2
+    # tarefa 2
     python_task_2 = PythonOperator(task_id='python_task_2', python_callable=second_task)
 
-    # Task 3
+    # tarefa 3
     python_task_3 = PythonOperator(task_id='python_task_3', python_callable=third_task)
 
     bash_task_1 >> python_task_2 >> python_task_3
--- Card-12/Aula/test_dags/test_tst_dag_definition_aula.py
+++ Card-12/Aula/test_dags/test_tst_dag_definition_aula.py
@@ -3,17 +3,22 @@ import collections
 import pendulum
 from airflow.models import DagBag
 
+# fixture para obter a DAG tst_dag
 @pytest.fixture(scope="class")
 def dag(dagbag):
     return dagbag.get_dag('tst_dag')
 
+# define uma classe de testes para a definição da DAG tst_dag
 class TestTstDagDefinition:
 
+    # constantes usadas nos testes
     EXPECTED_NB_TASKS = 6
     EXPECTED_TASKS = ['task_1', 'task_2', 'task_3', 'task_4', 'task_5', 'task_6']
 
+    # função auxiliar para comparar listas independentemente da ordem 
     compare = lambda self, x, y: collections.Counter(x) == collections.Counter(y)
 
+    # teste para verificar o número de tarefas na DAG
     def test_nb_tasks(self, dag):
         """
             Verify the number of tasks in the DAG
@@ -21,6 +26,7 @@ class TestTstDagDefinition:
         nb_tasks = len(dag.tasks)
         assert nb_tasks == self.EXPECTED_NB_TASKS, "Wrong number of tasks, {0} expected, got {1}".format(self.EXPECTED_NB_TASKS, nb_tasks)
 
+    # teste para verificar se a DAG contém as tarefas esperadas
     def test_contain_tasks(self, dag):
         """
             Verify if the DAG is composed of the expected tasks
@@ -28,6 +34,7 @@ class TestTstDagDefinition:
         task_ids = list(map(lambda task: task.task_id, dag.tasks))
         assert self.compare(task_ids, self.EXPECTED_TASKS)
 
+    # teste para verificar as dependências entre as tarefas
     @pytest.mark.parametrize("task, expected_upstream, expected_downstream", 
         [
             ("task_1", [], ["task_2"]), 
@@ -35,6 +42,8 @@ class TestTstDagDefinition:
             ("task_3", ["task_2"], ["task_6"])
         ]
     )
+
+    # teste para verificar as dependências entre as tarefas
     def test_dependencies_of_tasks(self, dag, task, expected_upstream, expected_downstream):
         """
             Verify if a given task has the expected upstream and downstream dependencies
@@ -44,12 +53,14 @@ class TestTstDagDefinition:
         assert self.compare(task.upstream_task_ids, expected_upstream), "The task {0} doesn't have the expected upstream dependencies".format(task)
         assert self.compare(task.downstream_task_ids, expected_downstream), "The task {0} doesn't have the expected downstream dependencies".format(task)
 
+    # teste para verificar se a data de início é anterior à data atual e se o catchup está desativado
     def test_start_date_and_catchup(self, dag):
         """
             Verify that the start_date is < current date and catchup = False
         """
         True
 
+    # teste para verificar se todas as tarefas têm a mesma data de início
     def test_same_start_date_all_tasks(self, dag):
         """
             Best Practice: All of your tasks should have the same start_date
